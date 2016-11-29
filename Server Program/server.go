994c5fe3359d540e7632f4aae18fc454fa386261
Myke Walker <mykewalker@gmail.com>
2016-11-28 21:24:06 -0800

    test commit from vscode editor

--- Server Program/server.go
+++ Server Program/server.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	fmt.Println("Server starting up")
-	go UDPServer("8081")
-	go TCPServer("8082")
+	go UDPServer(":8085")
+	go TCPServer(":8082")
 
 	for {
 
@@ -21,8 +21,9 @@ func TCPServer(port string) {
 	fmt.Println("in TCP Server")
 	ln, err := net.Listen("tcp", port)
 	if err != nil {
-		fmt.Println("error starting up TCPServer on Port: ", port)
+		fmt.Println("error starting up TCPServer on Port: ", port, err)
 	}
+	fmt.Println("waiting for TCP INFO")
 
 	connection, err := ln.Accept()
 	if err != nil {
@@ -30,6 +31,8 @@ func TCPServer(port string) {
 	}
 
 	for {
+		fmt.Println("receiving tcp input")
+
 		message, err := bufio.NewReader(connection).ReadString('\n')
 		if err != nil {
 			fmt.Println("error reading in a string ")
@@ -44,24 +47,25 @@ func TCPServer(port string) {
 func UDPServer(port string) {
 
 	fmt.Println("in UDP Server")
-	ln, err := net.Listen("UDP", port)
+	serveraddr, err := net.ResolveUDPAddr("udp", port)
+
+	ln, err := net.ListenUDP("udp", serveraddr)
 	if err != nil {
-		fmt.Println("error starting up UDPServer on Port: ", port)
+		fmt.Println("error starting up UDPServer on Port: ", port, err)
 	}
 
-	connection, err := ln.Accept()
-	if err != nil {
-		fmt.Println("error Accepting in UDPServer on Port: ", port)
-	}
+	buf := make([]byte, 1024)
 
 	for {
-		message, err := bufio.NewReader(connection).ReadString('\n')
+		fmt.Println("waiting for UDP INFO")
+
+		n, addr, err := ln.ReadFromUDP(buf)
+		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+
 		if err != nil {
-			fmt.Println("error reading in a string ")
-			fmt.Print("Message Received:", string(message))
+			fmt.Println("Error: ", err)
 
 		}
-
 	}
 
 }
