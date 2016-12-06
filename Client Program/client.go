c01bbc7a430b9ef5c6184ec0278dbc46125092b2
Myke Walker <mykewalker@gmail.com>
2016-12-05 21:45:59 -0800

    progress on server & client, server crashing

--- Client Program/client.go
+++ Client Program/client.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"os"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -15,12 +15,20 @@ import (
 //Packet struct is a datatype to hold a header number to keep track of operations
 //and Data is the string sent
 type Packet struct {
+	conn      net.Conn
+	ConnType  string
 	HeaderNum int    `json:"headernum"`
 	Data      string `json:"data"`
 }
 
+type LostPackets struct {
+	Packets []int `json:"packets"`
+}
+
+var resendlist = make(chan Packet)
 var wg sync.WaitGroup
-var MAXPACKETS = 10000
+var MAXPACKETS = 7000
+var numretry, packetsreceived []int32
 
 func main() {
 	fmt.Println("Entering Client Program for CSCI 156 Project")
@@ -39,6 +47,7 @@ func main() {
 	go TCPConnect(TCPIP)
 	go UDPConnect(UDPIP)
 	wg.Add(2)
+	// wg.Add(1)
 	wg.Wait()
 
 }
@@ -50,22 +59,41 @@ func TCPConnect(TCPIP string) {
 		log.Println("error while dialing TCP", err, TCPIP)
 	}
 
-	for k := 0; k < MAXPACKETS; k++ {
-		// for k := 0; k < 100; k++ {
+	fmt.Println("Initiating TCP Connection to Server")
+	for {
 
-		tcpconn.Write(makedataforpacket("obj number: " + strconv.FormatInt(int64(k), 10) + " HELLO TCP!\n"))
-		fmt.Println("sent data in TCP")
-		message, _ := bufio.NewReader(tcpconn).ReadString('\n')
-		fmt.Println("Received: ", message)
-
-		// buf := make([]byte, 1024)
-		// n, addr, err := udpconn.ReadFromUDP(buf)
+		// message, err := bufio.NewReader(tcpconn).ReadString('\n')
 		// if err != nil {
-		// 	log.Println("error reading UDP", err, UDPIP)
+		// 	log.Println("error message, err := bufio.NewReader(tcpconn).ReadString('\n')", err)
 		// }
-		// fmt.Println("Received ", string(buf[0:n]), " from ", addr)
 
-		time.Sleep(5 * time.Millisecond)
+		// fmt.Println("Received: ", message)
+
+		// // buf := make([]byte, 1024)
+		// // n, addr, err := udpconn.ReadFromUDP(buf)
+		// // if err != nil {
+		// // 	log.Println("error reading UDP", err, UDPIP)
+		// // }
+		// // fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+
+		decoder := json.NewDecoder(tcpconn)
+		var tmplist LostPackets
+
+		err := decoder.Decode(&tmplist)
+		if err != nil {
+			fmt.Printf("Client disconnected.\n")
+			break
+		}
+		for _, k := range tmplist.Packets {
+			tmppacket := Packet{
+				HeaderNum: k,
+				Data:      "this is misc packetdata",
+				ConnType:  "UDP",
+			}
+
+			resendlist <- tmppacket
+
+		}
 
 	}
 
@@ -76,37 +104,73 @@ func TCPConnect(TCPIP string) {
 	defer wg.Done()
 }
 
+func resendpackets(conn net.Conn) {
+	for {
+		//wait for data to resend
+		tmppacket := <-resendlist
+
+		tmpdata, err := json.Marshal(&tmppacket)
+		if err != nil {
+			log.Println("error Json.Marshal(tmppacket)", err)
+		}
+
+		_, err = conn.Write(tmpdata)
+		fmt.Println("Resending Packet num:", tmppacket)
+		if err != nil {
+			log.Println("error conn.Write(tmpdata)", err)
+		}
+		time.Sleep(3 * time.Millisecond)
+	}
+
+}
+
 //UDPConnect is a function that creates a TCP connection, and sends a small message
 func UDPConnect(UDPIP string) {
 	udpconn, err := net.Dial("udp", UDPIP)
 	if err != nil {
 		log.Println("error while dialing TCP", err, UDPIP)
 	}
-	udpconn.Write(makedataforpacket("HELLO UDP!\n"))
 	fmt.Println("sent data in UDP")
+	go resendpackets(udpconn)
 
 	for k := 0; k < MAXPACKETS; k++ {
+		tmppacket := &Packet{
+			HeaderNum: k,
+			Data:      "this is misc packetdata",
+			ConnType:  "UDP",
+		}
 
-		udpconn.Write(makedataforpacket("obj number: " + strconv.FormatInt(int64(k), 10) + "HELLO UDP!\n"))
-		time.Sleep(5 * time.Millisecond)
+		tmpdata, err := json.Marshal(tmppacket)
+		if err != nil {
+			log.Println("error Json.Marshal(tmppacket)", err)
+		}
+
+		if rand.Intn(100) > 90 {
+			fmt.Println("simulated packet loss")
+			//do nothing
+		} else {
+			udpconn.Write(makedataforpacket(tmpdata))
+			fmt.Println("sent data in UDP")
+		}
+
+		time.Sleep(1 * time.Millisecond)
 
 	}
 
-	err = udpconn.Close()
-	if err != nil {
-		log.Println("Closing Connection", err, UDPIP)
-	}
+	// err = udpconn.Close()
+	// if err != nil {
+	// 	log.Println("Closing Connection", err, UDPIP)
+	// }
 	defer wg.Done()
 }
 
 //function makedataforpacket receives an integer index number,
 //converts that number to bytes, then places it in a
 //1000 byte buffer and returns it
-func makedataforpacket(index string) []byte {
-	//convert int to slice of bytes
-	indexdata := []byte(index)
+func makedataforpacket(data []byte) []byte {
+
 	//place that slice of bytes into a buffer
-	bytebuffer := bytes.NewBuffer(indexdata)
+	bytebuffer := bytes.NewBuffer(data)
 	return bytebuffer.Bytes()
 
 }
--- Server Program/server.go
+++ Server Program/server.go
@@ -1,24 +1,38 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"sync"
+	"time"
 )
+
+type LostPackets struct {
+	Packets []int `json:"packets"`
+}
 
 //Packet struct is a datatype to hold a header number to keep track of operations
 //and Data is the string sent
-type Packet struct {
+type UDPPacket struct {
+	Addr      *net.UDPAddr
+	Conn      net.Conn
 	HeaderNum int    `json:"headernum"`
 	Data      string `json:"data"`
 }
 
+var numpacketsrange = 20
 var mainwg sync.WaitGroup
 var tcpwg sync.WaitGroup
 var udpwg sync.WaitGroup
+var ackchan = make(chan int)
+var resendchan = make(chan int)
+var packets [10000]bool
+var resendmap = make(map[int]bool, 11000)
 
 func main() {
+	go HandleIncomingACKs()
 	fmt.Println("Server starting up")
 	go UDPServer(":8085")
 	go TCPServer(":8082")
@@ -27,8 +41,44 @@ func main() {
 
 }
 
+func StatusOfTransmission(conn net.Conn) {
+	fmt.Println("Entered Status of Transmission Func, entering while loop")
+
+	for {
+		var resendlist []int
+		fmt.Println("Number of missing packets", len(resendmap))
+		fmt.Println("Missing Packets: ", resendmap)
+
+		if len(resendmap) > 0 {
+
+			for k := range resendmap {
+				resendlist = append(resendlist, k)
+			}
+
+			tmplist := LostPackets{
+				Packets: resendlist,
+			}
+
+			tmpdata, err := json.Marshal(tmplist)
+			if err != nil {
+				log.Println("error Json.Marshal(tmppacket)", err)
+			}
+
+			_, err = conn.Write(tmpdata)
+			if err != nil {
+				log.Println("error conn.Write(tmpdata)", err)
+			}
+		}
+
+		time.Sleep(15 * time.Second)
+
+	}
+
+}
+
 //TCPServer is a function to handle incoming TCP connections
 func TCPServer(port string) {
+	defer mainwg.Done()
 
 	fmt.Println("in TCP Server")
 	ln, err := net.Listen("tcp", port)
@@ -37,67 +87,140 @@ func TCPServer(port string) {
 	}
 	fmt.Println("waiting for TCP INFO")
 
-	//sets the last the program will do before exiting, close the connections
+	//sets the last the func will do before exiting, close the connections
 	defer ln.Close()
 
-	connection, err := ln.Accept()
-	if err != nil {
-		fmt.Println("error Accepting in TCPServer on Port: ", port)
-	}
-	go HandleIncomingTCPData(connection)
-	tcpwg.Add(1)
-	tcpwg.Wait()
+	for {
+		connection, err := ln.Accept()
+		if err != nil {
+			fmt.Println("error Accepting in TCPServer on Port: ", port)
+			break
+		}
+		fmt.Println("Accepted connection", connection)
+		go StatusOfTransmission(connection)
 
-	defer mainwg.Done()
+		go func() {
+
+			for {
+				//maintain open connection
+
+			}
+		}()
+
+	}
 
 }
 
 //HandleIncomingTCPData is a function to deal with the incoming TCP connection
-func HandleIncomingTCPData(connection net.Conn) {
-	// message := ""
-
+func HandleIncomingData(Packets chan UDPPacket) {
+	fmt.Println("preparing to handle incoming tcp data, entering loop")
 	for {
+		// Wait for the next job to come off the queue.
+		tmppacket := <-Packets
+		// fmt.Println("got next packet off of the packet channel")
 
-		message, _ := bufio.NewReader(connection).ReadString('\n')
-		// if err != nil {
-		// 	break
-		// }
-		if message != "" {
-			fmt.Print("Message Received:", string(message))
-			connection.Write([]byte("Received, and redirecting:" + message))
+		// fmt.Println("Received:", tmppacket)
+
+		// Send back the response.
+		//prepare data for TCP ACK
+		ackchan <- tmppacket.HeaderNum
+
+	}
+
+}
+
+//HandleIncomingACKs is a function to deal with the incoming TCP connection
+func HandleIncomingACKs() {
+	fmt.Println("preparing to handle ACKS, entering loop")
+	for {
+		// Wait for the next job to come off the queue.
+		tmpacknum := <-ackchan
+		// fmt.Println("got ack from UDP Data")
+		packets[tmpacknum] = true
+
+		if resendmap[tmpacknum] == true {
+			delete(resendmap, tmpacknum)
 		}
 
+		//every numpacketsrange packets, run the
+		if tmpacknum%numpacketsrange == 0 {
+
+			for j := 0; j <= 19; j++ {
+				if (tmpacknum-j > 0) && (packets[tmpacknum-j] == false) {
+					fmt.Println("Need Packet Num", tmpacknum-j)
+
+					resendmap[tmpacknum-j] = true
+
+				}
+
+			}
+
+		}
 	}
 
 }
 
 //UDPServer is a function to handle incoming TCP connections
 func UDPServer(port string) {
+	defer mainwg.Done()
+	Packets := make(chan UDPPacket)
 
 	fmt.Println("in UDP Server")
-	serveraddr, err := net.ResolveUDPAddr("udp", port)
 
-	ln, err := net.ListenUDP("udp", serveraddr)
+	ln, err := net.ResolveUDPAddr("udp4", port)
 	if err != nil {
-		fmt.Println("error starting up UDPServer on Port: ", port, err)
+		fmt.Println("error starting up UDP on Port: ", port, err)
 	}
 
-	//sets the last the program will do before exiting, close the connections
-	defer ln.Close()
+	go HandleIncomingData(Packets)
+	conn, err := net.ListenUDP("udp", ln)
+	if err != nil {
+		fmt.Println("error starting up UDP on Port: ", port, err)
+	}
+	udpwg.Add(1)
+	err = conn.SetReadBuffer(10000)
+	if err != nil {
+		log.Println("	err = conn.SetReadBuffer(10000)", err)
+	}
+	err = conn.SetWriteBuffer(10000)
+	if err != nil {
+		log.Println("err = conn.SetReadBuffer(10000)", err)
+	}
 
-	buf := make([]byte, 1024)
+	go func() {
+		buf := make([]byte, 1024)
 
-	for {
-		fmt.Println("waiting for UDP INFO")
+		for {
 
-		n, addr, err := ln.ReadFromUDP(buf)
-		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+			n, _, err := conn.ReadFrom(buf)
+			if err != nil {
+				fmt.Printf("Client disconnected.\n")
+				fmt.Println(err)
+				break
+			}
+			var tmppacket UDPPacket
 
-		if err != nil {
-			fmt.Println("Error: ", err)
+			err = json.Unmarshal(buf[0:n], &tmppacket)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			tmppacket.Conn = conn
+			tmppacket.Addr = ln
+
+			Packets <- tmppacket
 
 		}
-	}
-	defer mainwg.Done()
+	}()
+	udpwg.Wait()
 
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
